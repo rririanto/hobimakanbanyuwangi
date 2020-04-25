64fece050a01b3989c73d672e84e7295cfb5d605
Rahmat Ramadhan <romanticdevil.jimmy@gmail.com>
2020-04-25 11:35:42 +0700

    chore(server): add MEDIA_URL into

--- config/settings/production.py
+++ config/settings/production.py
@@ -14,6 +14,8 @@ DATABASES["default"] = env.db("DATABASE_URL")  # noqa F405
 DATABASES["default"]["ATOMIC_REQUESTS"] = True  # noqa F405
 DATABASES["default"]["CONN_MAX_AGE"] = env.int("CONN_MAX_AGE", default=60)  # noqa F405
 
+#MEDIA
+MEDIA_URL = "/hobimakanbanyuwangi/media/"
 
 # SECURITY
 # ------------------------------------------------------------------------------
